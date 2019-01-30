07f4a3088d9165ab68d8445dc398fc18cf15aebf
Travis Gee <geet084@gmail.com>
2019-01-30 15:09:17 -0700

    Added Router

--- src/index.js
+++ src/index.js
@@ -6,13 +6,14 @@ import * as serviceWorker from './serviceWorker';
 import rootReducer from './reducers';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { BrowserRouter} from 'react-router-dom'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 const store = createStore(rootReducer, devTools);
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <BrowserRouter><App /></BrowserRouter>
   </Provider>, 
 document.getElementById('root'));
 
