b97402ec50b5da2703f1cefede3af2a8405ed21d
Travis Gee <geet084@gmail.com>
2019-01-30 15:47:47 -0700

    Add a controlled form for the signup page

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -2,14 +2,62 @@ import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 
 class Login extends Component {
+  constructor() {
+    super()
+    this.state = {
+      name: '',
+      email: '',
+      password: '',
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    
+    
+    this.setState({
+      name: '',
+      email: '',
+      password: ''
+    })
+  }
+
+  handleChange = (e) => {
+    let { name, value } = e.target;
+    this.setState({ [name]: value })
+  }
 
   render() {
+    const {name, email, password} = this.state
     return (
       <div>
         <Link to='/'>HOME</Link>
-        <h1>
-        login page? 
-        </h1>
+        <form onSubmit={this.handleSubmit}>
+          <label>Name
+            <input
+              onChange={this.handleChange}
+              name="name"
+              type="text"
+              value={name} />
+          </label>
+          <label>Email
+            <input
+              onChange={this.handleChange}
+              name="email"
+              type="email"
+              value={email} />
+          </label>
+          <label>Password
+            <input
+              onChange={this.handleChange}
+              name="password"
+              type="password"
+              value={password} />
+          </label>
+          <input type="submit"/>
+        </form>
+
+        <Link to='/'>sign up page</Link>
       </div>
     )
   }
