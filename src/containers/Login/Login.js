09262f51e3274e4cb3b93a2dfbdd77bf870be200
Travis Gee <geet084@gmail.com>
2019-01-30 15:09:36 -0700

    Add Login page

---
+++ src/containers/Login/Login.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+
+class Login extends Component {
+
+  render() {
+    return (
+      <div>
+        <Link to='/'>HOME</Link>
+        <h1>
+        login page? 
+        </h1>
+      </div>
+    )
+  }
+}
+
+export default Login;
