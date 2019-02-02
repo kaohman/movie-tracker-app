e86f996533ddb983713a03a4714015e8ce99f4ab
Karin Ohman <kaohma@gmail.com>
2019-02-02 12:49:08 -0700

    Add loading functionality on App

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import '../../normalize.css';
 import '../../main.scss';
 import { connect } from 'react-redux';
-import { setMovies, errorToDisplay, setCurrentUser } from '../../actions';
+import { setMovies, errorToDisplay, setCurrentUser, isLoading } from '../../actions';
 import API from '../../utils/api';
 import { apiKey } from '../../utils/api-key';
 import Movies from '../Movies/Movies'
@@ -14,13 +14,14 @@ import MovieDetails from '../../components/MovieDetails/MovieDetails'
 export class App extends Component {
   
   componentDidMount = async () => {
-    const { errorToDisplay, setMovies } = this.props
+    const { errorToDisplay, setMovies, isLoading } = this.props
     const initialCategory = 'popular'
     const root = `https://api.themoviedb.org/3/movie/${initialCategory}`
     const url = `${root}?page=1&api_key=${apiKey}&language=en-US`
     try {
       const movies = await API.getData(url);
       await setMovies(movies.results);
+      isLoading(false);
     } catch (error) {
       errorToDisplay(error)
     }
@@ -31,51 +32,57 @@ export class App extends Component {
   }
 
   render() {
-    return (
-      <div className="App">
-        <header>
-          <h1>Movie Tracker</h1>
-          <div>
-            {
-              this.props.user.name ? <span className="user-name">Welcome: {this.props.user.name}... </span> : ''
+    if (this.props.loading) {
+      return (<h1>Loading Movies...</h1>)
+    } else {
+      return (
+        <div className="App">
+          <header>
+            <h1>Movie Tracker</h1>
+            <div>
+              {
+                this.props.user.name ? <span className="user-name">Welcome: {this.props.user.name}... </span> : ''
+              }
+              <NavLink activeClassName='selected' className='nav-links' to='/'>Popular Movies</NavLink>
+              <NavLink activeClassName='selected' className='nav-links' to='/favorites'>Favorites</NavLink>
+              {
+                this.props.user.name ?
+                  <button className='nav-links' id='sign-out-button' onClick={this.signOut}>Sign Out</button> :
+                  <NavLink className='nav-links' to="/login">User Login</NavLink>
+              }
+            </div>
+          </header>
+          <Route exact path='/' component={Movies} />
+          <Route exact path='/favorites' render={() => {
+            return <Movies location={this.props.location} />
+          }} />
+          <Route path='/movies/:id' render={({ match }) => {
+            const { id } = match.params;
+            const movie = this.props.movies.find(movie => movie.id === parseInt(id))
+            
+            if (movie) {
+              return <MovieDetails {...movie} />
             }
-            <NavLink activeClassName='selected' className='nav-links' to='/'>Popular Movies</NavLink>
-            <NavLink activeClassName='selected' className='nav-links' to='/favorites'>Favorites</NavLink>
-            {
-              this.props.user.name ?
-                <button className='nav-links' id='sign-out-button' onClick={this.signOut}>Sign Out</button> :
-                <NavLink className='nav-links' to="/login">User Login</NavLink>
-            }
-          </div>
-        </header>
-        <Route exact path='/' component={Movies} />
-        <Route exact path='/favorites' render={() => {
-          return <Movies location={this.props.location} />
-        }} />
-        <Route path='/movies/:id' render={({ match }) => {
-          const { id } = match.params;
-          const movie = this.props.movies.find(movie => movie.id === parseInt(id))
-          
-          if (movie) {
-            return <MovieDetails {...movie} />
-          }
-        }} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/signup' component={SignUp} />
-      </div>
-    );
+          }} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/signup' component={SignUp} />
+        </div>
+      )
+    }
   }
 }
 
 export const mapStateToProps = (state) => ({
   user: state.user,
   movies: state.movies,
+  loading: state.status,
 })
 
 export const mapDispatchToProps = (dispatch) => ({
   setMovies: (movies) => dispatch(setMovies(movies)),
   setCurrentUser: (user) => dispatch(setCurrentUser(user)),
   errorToDisplay: (message) => dispatch(errorToDisplay(message)),
+  isLoading: (status) => dispatch(isLoading(status)),
 })
 
 export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
